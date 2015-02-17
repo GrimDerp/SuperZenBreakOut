c01b1c2b1cf040b6db27deaf2a2f972b4ea24cee
BC <bcillustration1@gmail.com>
2015-02-16 17:34:42 -0700

    third attempt.

---
+++ Assets/Runner/Runner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Runner : MonoBehaviour {
+	
+	public static float distanceTraveled;
+	public Vector3 jumpVelocity;
+	public float acceleration;
+	private bool touchingPlatform;
+	
+	void Update () {
+		if(touchingPlatform && Input.GetButtonDown("Jump")){
+			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+		}
+		distanceTraveled = transform.localPosition.x;
+	}
+	
+	void FixedUpdate () {
+		if(touchingPlatform){
+			rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+		}
+	}
+	
+	void OnCollisionEnter () {
+		touchingPlatform = true;
+	}
+	
+	void OnCollisionExit () {
+		touchingPlatform = false;
+	}
+}
