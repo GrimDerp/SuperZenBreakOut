607e8943cda289a00f7d51b306c2f58770335b2f
BC <bcillustration1@gmail.com>
2015-07-10 16:54:19 -0600

    Updating Runner.cs
    Adding rotation... I hope

--- Assets/Runner/Runner.cs
+++ Assets/Runner/Runner.cs
@@ -5,11 +5,14 @@ public class Runner : MonoBehaviour {
 	public static float distanceTraveled;
 	public Vector3 jumpVelocity;
 	public float acceleration;
+	public float torque;
 	private bool touchingPlatform;
 	
 	void Update () {
 		if(touchingPlatform && Input.GetButtonDown("Jump")){
 			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+			float turn = Input.GetAxis("Horizontal");
+        		rb.AddRelativeTorque(Vector3.up * torque * turn);
 		}
 		distanceTraveled = transform.localPosition.x;
 	}
