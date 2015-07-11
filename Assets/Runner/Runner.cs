0116d5d393c8ce81083ced1d198efcd88683355f
BC <bcillustration1@gmail.com>
2015-07-11 06:50:45 -0600

    Updating Runner.cs
    now we're cookin with fire

--- Assets/Runner/Runner.cs
+++ Assets/Runner/Runner.cs
@@ -1,33 +1,90 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class Runner : MonoBehaviour {
-	
+
+	public Vector3 offset, rotationVelocity;
 	public static float distanceTraveled;
-	public Vector3 jumpVelocity;
+	public Vector3 boostVelocity, jumpVelocity;
 	public float acceleration;
-	public float torque;
+	public float gameOverY;
 	private bool touchingPlatform;
-	
-	void Update () {
-		if(touchingPlatform && Input.GetButtonDown("Jump")){
-			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
-			float turn = Input.GetAxis("Horizontal");
-        		rb.AddRelativeTorque(Vector3.up * torque * turn);
-		}
-		distanceTraveled = transform.localPosition.x;
-	}
-	
-	void FixedUpdate () {
-		if(touchingPlatform){
-			rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
-		}
-	}
-	
-	void OnCollisionEnter () {
+	private Vector3 startPosition;
+
+	private static int boosts;
+
+	void OnCollisionEnter(){
 		touchingPlatform = true;
 	}
 	
-	void OnCollisionExit () {
+	void OnCollisionExit(){
 		touchingPlatform = false;
 	}
-}
+	
+	void Update () {
+		transform.Rotate(rotationVelocity * Time.deltaTime);
+		if (Input.GetButtonDown ("Jump")) {
+					if (touchingPlatform) {
+							GetComponent<Rigidbody>().AddForce (jumpVelocity, ForceMode.VelocityChange);
+							touchingPlatform = false;
+							
+								
+						}
+						else if (boosts > 0) {
+								GetComponent<Rigidbody>().AddForce (boostVelocity, ForceMode.VelocityChange);
+								boosts -= 1;
+								GUIManager.SetBoosts(boosts);
+						}
+				}
+			distanceTraveled = transform.localPosition.x;
+			GUIManager.SetDistance (distanceTraveled);
+
+
+			if(transform.localPosition.y < gameOverY){
+				GameEventManager.TriggerGameOver();
+			}
+		}
+	
+		private void GameStart () {
+			boosts = 3;
+			GUIManager.SetBoosts(boosts);
+			distanceTraveled = 0f;
+			GUIManager.SetDistance(distanceTraveled);
+			transform.localPosition = startPosition;
+			GetComponent<Renderer>().enabled = true;
+			GetComponent<Rigidbody>().isKinematic = false;
+			enabled = true;
+		}
+
+	public static void AddBoost(){
+		boosts += 1;
+		GUIManager.SetBoosts(boosts);
+
+	}
+
+	private void GameOver () {
+		GetComponent<Renderer>().enabled = false;
+		GetComponent<Rigidbody>().isKinematic = true;
+		enabled = false;
+		
+	}
+
+	void fixedUpdate (){
+		if (touchingPlatform) {
+			GetComponent<Rigidbody>().AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);		
+		}
+	}
+
+
+
+	void Start () {
+				GameEventManager.GameStart += GameStart;
+				GameEventManager.GameOver += GameOver;
+				startPosition = transform.localPosition;
+				GetComponent<Renderer>().enabled = false;
+				GetComponent<Rigidbody>().isKinematic = true;
+				enabled = false;
+		}
+
+	}
+
+	
